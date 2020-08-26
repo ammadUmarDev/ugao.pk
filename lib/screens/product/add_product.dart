2175b12dba7ecb04a4dfe87a002003ea77764be0
Zubaria Ayub <zubariaayub634@gmail.com>
2020-08-26 17:50:15 +0500

    Created input text fields and TODOs for other fields

--- lib/screens/product/add_product.dart
+++ lib/screens/product/add_product.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../components/drop_down_field.dart';
+import '../../components/rounded_button.dart';
+import '../../components/rounded_input_field.dart';
+import '../../components/rounded_input_field.dart';
+import '../../components/rounded_input_field.dart';
+import '../../components/rounded_input_field.dart';
+import '../../components/rounded_input_field.dart';
+
 class AddProduct extends StatefulWidget {
   @override
   _AddProductState createState() => _AddProductState();
@@ -14,7 +22,37 @@ class _AddProductState extends State<AddProduct> {
         child: Container(
           child: Stack(
             children: [
-              Text("Add a product"),
+              ListView(
+                children: [
+                  RoundedInputField(
+                    hintText: "Product Name",
+                    icon: Icons.keyboard,
+                  ),
+                  RoundedInputField(
+                    hintText: "Product Description",
+                    icon: Icons.keyboard,
+                  ),
+                  //TODO: add dropdown to select type of price here. options: ["Per Unit Price", "Bulk Price"]
+                  RoundedInputField(
+                    //TODO: limit to integer only input
+                    hintText: "Price",
+                    icon: Icons.keyboard, //TODO: find appropriate icon
+                  ),
+                  RoundedInputField(
+                    //TODO: limit to integer only input
+                    hintText: "Quantity",
+                    icon: Icons.keyboard,
+                  ),
+                  RoundedInputField(
+                    //TODO: limit to integer only input
+                    //TODO: add option to select weight unit
+                    hintText: "Weight",
+                    icon: Icons.keyboard, //TODO: find appropriate icon
+                  ),
+                  //TODO: add dropdown to select category
+                  //TODO: create image picker for product image
+                ],
+              ),
             ],
           ),
         ),
