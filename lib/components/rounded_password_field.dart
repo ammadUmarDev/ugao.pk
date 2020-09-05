eaae0577dd1c78eee33c4ed9130d240cb68bf13a
zubariaayub634 <zubariaayub634@gmail.com>
2020-09-05 17:40:43 +0500

    Toggle obscurity in password field

--- lib/components/rounded_password_field.dart
+++ lib/components/rounded_password_field.dart
@@ -2,32 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:ugao/components/text_field_container.dart';
 import '../constants.dart';
 
-class RoundedPasswordField extends StatelessWidget {
+class RoundedPasswordField extends StatefulWidget {
   final ValueChanged<String> onChanged;
   const RoundedPasswordField({
     Key key,
     this.onChanged,
   }) : super(key: key);
+  @override
+  _RoundedPasswordFieldState createState() => _RoundedPasswordFieldState();
+}
+
+class _RoundedPasswordFieldState extends State<RoundedPasswordField> {
+
+  bool _obscureText=true;
 
   @override
   Widget build(BuildContext context) {
     return TextFieldContainer(
-      child: TextField(
-        obscureText: true,
-        onChanged: onChanged,
-        cursorColor: kPrimaryAccentColor,
-        decoration: InputDecoration(
-          hintText: "Password",
-          icon: Icon(
-            Icons.lock,
-            color: kPrimaryAccentColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(
+            child: TextField(
+              obscureText: _obscureText,
+              onChanged: widget.onChanged,
+              cursorColor: kPrimaryAccentColor,
+              decoration: InputDecoration(
+                hintText: "Password",
+                icon: Icon(
+                  Icons.lock,
+                  color: kPrimaryAccentColor,
+                ),
+                border: InputBorder.none,
+              ),
+            ),
           ),
-          suffixIcon: Icon(
-            Icons.visibility,
-            color: kPrimaryAccentColor,
+          IconButton(
+            icon: Icon(
+              _obscureText?Icons.visibility:Icons.visibility_off,
+              color: kPrimaryAccentColor,
+            ),
+            onPressed: () {
+              setState(() {
+                _obscureText=!_obscureText;
+              });
+            },
           ),
-          border: InputBorder.none,
-        ),
+        ],
       ),
     );
   }
