f27341282e3bb1f45d1b071c2aed0bc4455cf197
zubariaayub634 <zubariaayub634@gmail.com>
2020-09-08 10:13:33 +0500

    Allows alteration of quantity on cart page. Replaces JSON with CartProductModel

--- lib/screens/cart/cart_products.dart
+++ lib/screens/cart/cart_products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ugao/Classes/Cart_Product_Model.dart';
 import 'package:ugao/Providers/product_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:ugao/Classes/User_Model.dart';
@@ -10,21 +11,31 @@ class Cart_products extends StatefulWidget {
 }
 
 class _Cart_productsState extends State<Cart_products> {
-  var Products_on_the_cart = [
-    {
-      "name": "Fertilizer",
-      "picture": "assets/images/bgwelcome.png",
-      "price": 3000,
-      "quantity": 1,
-      "service": "Pickup",
-    },
-    {
-      "name": "Rice",
-      "picture": "assets/images/clothes.jpeg",
-      "price": 200,
-      "quantity": 2,
-      "service": "Delivery",
-    },
+  List<CartProduct> Products_on_the_cart = [
+    CartProduct(
+        product: ProductFetch(
+            prodName: "Green Up Lawn Fertilizer",
+            prodImage:
+                "https://firebasestorage.googleapis.com/v0/b/ugao-58f32.appspot.com/o/data%2Fuser%2F0%2Fcom.vectech.ugao%2Fcache%2Fimage_picker2573009106067769771.jpg?alt=media&token=06f7a0c2-7cfb-4855-9b3b-3246e710331d",
+            price: 4000),
+        quantity: 5,
+        serviceType: "Delivery"),
+    CartProduct(
+        product: ProductFetch(
+            prodName: "Green Up Lawn Fertilizer",
+            prodImage:
+                "https://firebasestorage.googleapis.com/v0/b/ugao-58f32.appspot.com/o/data%2Fuser%2F0%2Fcom.vectech.ugao%2Fcache%2Fimage_picker2573009106067769771.jpg?alt=media&token=06f7a0c2-7cfb-4855-9b3b-3246e710331d",
+            price: 4000),
+        quantity: 2,
+        serviceType: "Pickup"),
+    CartProduct(
+        product: ProductFetch(
+            prodName: "Green Up Lawn Fertilizer",
+            prodImage:
+                "https://firebasestorage.googleapis.com/v0/b/ugao-58f32.appspot.com/o/data%2Fuser%2F0%2Fcom.vectech.ugao%2Fcache%2Fimage_picker2573009106067769771.jpg?alt=media&token=06f7a0c2-7cfb-4855-9b3b-3246e710331d",
+            price: 4000),
+        quantity: 5,
+        serviceType: "Delivery"),
   ];
 
   @override
@@ -36,22 +47,22 @@ class _Cart_productsState extends State<Cart_products> {
         itemCount: Products_on_the_cart.length,
         itemBuilder: (context, index) {
           return Single_cart_product(
-            cart_prod_name: Products_on_the_cart[index]["name"],
-            cart_prod_picture: Products_on_the_cart[index]["picture"],
-            cart_prod_price: Products_on_the_cart[index]["price"],
-            cart_prod_qty: Products_on_the_cart[index]["quantity"],
-            cart_prod_service: Products_on_the_cart[index]["service"],
+            cart_prod_name: Products_on_the_cart[index].product.prodName,
+            cart_prod_picture: Products_on_the_cart[index].product.prodImage,
+            cart_prod_price: Products_on_the_cart[index].product.price,
+            cart_prod_qty: Products_on_the_cart[index].quantity,
+            cart_prod_service: Products_on_the_cart[index].serviceType,
           );
         });
   }
 }
 
-class Single_cart_product extends StatelessWidget {
+class Single_cart_product extends StatefulWidget {
   final cart_prod_name;
   final cart_prod_picture;
   final cart_prod_price;
   final cart_prod_service;
-  var cart_prod_qty;
+  final cart_prod_qty;
 
   Single_cart_product({
     this.cart_prod_name,
@@ -60,111 +71,101 @@ class Single_cart_product extends StatelessWidget {
     this.cart_prod_qty,
     this.cart_prod_service,
   });
+  @override
+  _Single_cart_productState createState() =>
+      _Single_cart_productState(this.cart_prod_qty);
+}
 
+class _Single_cart_productState extends State<Single_cart_product> {
+  int cart_prod_qty;
+
+  _Single_cart_productState(this.cart_prod_qty);
   void incQuantity() {
-    this.cart_prod_qty = this.cart_prod_qty + 1;
+    setState(() {
+      this.cart_prod_qty = this.cart_prod_qty + 1;
+    });
   }
 
   void decQuantity() {
-    this.cart_prod_qty = this.cart_prod_qty - 1;
+    setState(() {
+      this.cart_prod_qty = this.cart_prod_qty - 1;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: ListTile(
-            // Leading Section
-            leading: new Image.asset(
-              "assets/images/fertilizer1.jpg",
-              width: 100.0,
-              height: 100.0,
-            ),
-            title: new Text(
-              cart_prod_name,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: new Column(
+      child: ListTile(
+        // Leading Section
+        leading: new Image.network(
+          widget.cart_prod_picture,
+          width: 100.0,
+          height: 100.0,
+        ),
+        title: new Text(
+          widget.cart_prod_name,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: new Column(
+          children: <Widget>[
+            //Row inside Column
+            new Row(
               children: <Widget>[
-                //Row inside Column
-                new Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
-                      child: new Text(cart_prod_service),
-                    ),
-                    /*Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: new Text(cart_prod_qty.toString()),
-                    ),*/
-                  ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+                  child: new Text(widget.cart_prod_service),
                 ),
-                new Row(
-                  children: <Widget>[
-                    //qty of product
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
-                      child: new Text("Quantity:"),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: new Text(cart_prod_qty.toString()),
-                    ),
-                  ],
-                ),
-                new Row(
-                  children: <Widget>[
-                    //price of product
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: new Text(
-                        "Rs." + (cart_prod_price * cart_prod_qty).toString(),
-                        style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green[300]),
-                      ),
-                    ),
-                  ],
-                )
               ],
             ),
-            trailing: FittedBox(
-              fit: BoxFit.fill,
-              child: Column(
-                children: <Widget>[
-                  // ======== inc Quantity dec Quantity ============
-                  new IconButton(
-                      icon: Icon(Icons.arrow_drop_up),
-                      onPressed: () {
-                        this.incQuantity();
-                      }),
-                  new IconButton(
-                      icon: Icon(Icons.arrow_drop_down),
-                      onPressed: () {
-                        this.decQuantity();
-                      }),
-                ],
-              ),
-            ) /*new Column(
-        children: <Widget>[
-          new Row(children: <Widget>[
-            Expanded(
-              child: new IconButton(
-                  icon: Icon(Icons.arrow_drop_up), onPressed: () {}),
+            new Row(
+              children: <Widget>[
+                //qty of product
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+                  child: new Text("Quantity:"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: new Text(cart_prod_qty.toString()),
+                ),
+              ],
+            ),
+            new Row(
+              children: <Widget>[
+                //price of product
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: new Text(
+                    "Rs." + (widget.cart_prod_price * cart_prod_qty).toString(),
+                    style: TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green[300]),
+                  ),
+                ),
+              ],
             )
-          ]),
-          new Row(
+          ],
+        ),
+        trailing: FittedBox(
+          fit: BoxFit.fill,
+          child: Column(
             children: <Widget>[
-              //Padding(
-              //padding: const EdgeInsets.all(1.0),
-              //child:
+              // ======== inc Quantity dec Quantity ============
               new IconButton(
-                  icon: Icon(Icons.arrow_drop_down), onPressed: () {}),
-              //)
+                  icon: Icon(Icons.arrow_drop_up),
+                  onPressed: () {
+                    this.incQuantity();
+                  }),
+              new IconButton(
+                  icon: Icon(Icons.arrow_drop_down),
+                  onPressed: () {
+                    this.decQuantity();
+                  }),
             ],
-          )
-        ],
-      ),*/
-            ));
+          ),
+        ),
+      ),
+    );
   }
 }
