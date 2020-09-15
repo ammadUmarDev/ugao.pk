be9dcbdf454a848b5cf2e544f1661ba67cbede49
zubariaayub634 <zubariaayub634@gmail.com>
2020-09-15 12:33:52 +0500

    Disables back button for Welcome Screen

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -4,8 +4,11 @@ import 'components/body.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Body(),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Body(),
+      ),
     );
   }
 }
