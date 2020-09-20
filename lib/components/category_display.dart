249b6dbaaa69ce6b530fa7e34c9972f77e2db2e1
ammadUmarDev <ammad.umar@outlook.com>
2020-09-20 16:35:08 +0500

    overflow fix

--- lib/components/category_display.dart
+++ lib/components/category_display.dart
@@ -16,249 +16,257 @@ class CategoryDisplay extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Column(
-            children: <Widget>[
-              Column(
-                children: [
-                  Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductsScreen(
-                                    categoryName: "Machinery",
-                                  )),
-                        );
-                      },
-                      shape: CircleBorder(),
-                      child: Icon(
-                        FontAwesomeIcons.tractor,
-                        color: kPrimaryAccentColor,
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Column(
+                  children: [
+                    Container(
+                      width: width,
+                      height: height,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsScreen(
+                                      categoryName: "Machinery",
+                                    )),
+                          );
+                        },
+                        shape: CircleBorder(),
+                        child: Icon(
+                          FontAwesomeIcons.tractor,
+                          color: kPrimaryAccentColor,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  BodyText(textBody: "Machinery"),
-                ],
-              ),
-              SizedBox(height: 10),
-              Column(
-                children: [
-                  Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductsScreen(
-                                    categoryName: "Tools & Equipment",
-                                  )),
-                        );
-                      },
-                      shape: CircleBorder(),
-                      child: Icon(
-                        FontAwesomeIcons.toolbox,
-                        color: kPrimaryAccentColor,
+                    SizedBox(height: 5),
+                    BodyText(textBody: "Machinery"),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Column(
+                  children: [
+                    Container(
+                      width: width,
+                      height: height,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsScreen(
+                                      categoryName: "Tools & Equipment",
+                                    )),
+                          );
+                        },
+                        shape: CircleBorder(),
+                        child: Icon(
+                          FontAwesomeIcons.toolbox,
+                          color: kPrimaryAccentColor,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  BodyText(textBody: "Tools & Equipment"),
-                ],
-              ),
-            ],
+                    SizedBox(height: 5),
+                    BodyText(textBody: "Tools & Equipment"),
+                  ],
+                ),
+              ],
+            ),
           ),
-          Column(
-            children: <Widget>[
-              Column(
-                children: [
-                  Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductsScreen(
-                                    categoryName: "Pesticide",
-                                  )),
-                        );
-                      },
-                      shape: CircleBorder(),
-                      child: Icon(
-                        FontAwesomeIcons.bug,
-                        color: kPrimaryAccentColor,
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Column(
+                  children: [
+                    Container(
+                      width: width,
+                      height: height,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsScreen(
+                                      categoryName: "Pesticide",
+                                    )),
+                          );
+                        },
+                        shape: CircleBorder(),
+                        child: Icon(
+                          FontAwesomeIcons.bug,
+                          color: kPrimaryAccentColor,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  BodyText(textBody: "Pesticide"),
-                ],
-              ),
-              SizedBox(height: 10),
-              Column(
-                children: [
-                  Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductsScreen(
-                                    categoryName: "Fertilizer",
-                                  )),
-                        );
-                      },
-                      shape: CircleBorder(),
-                      child: Icon(
-                        FontAwesomeIcons.seedling,
-                        color: kPrimaryAccentColor,
+                    SizedBox(height: 5),
+                    BodyText(textBody: "Pesticide"),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Column(
+                  children: [
+                    Container(
+                      width: width,
+                      height: height,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsScreen(
+                                      categoryName: "Fertilizer",
+                                    )),
+                          );
+                        },
+                        shape: CircleBorder(),
+                        child: Icon(
+                          FontAwesomeIcons.seedling,
+                          color: kPrimaryAccentColor,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  BodyText(textBody: "Fertilizer"),
-                ],
-              ),
-            ],
+                    SizedBox(height: 5),
+                    BodyText(textBody: "Fertilizer"),
+                  ],
+                ),
+              ],
+            ),
           ),
-          Column(
-            children: <Widget>[
-              Column(
-                children: [
-                  Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductsScreen(
-                                    categoryName: "Livestock",
-                                  )),
-                        );
-                      },
-                      shape: CircleBorder(),
-                      child: Icon(
-                        FontAwesomeIcons.horseHead,
-                        color: kPrimaryAccentColor,
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Column(
+                  children: [
+                    Container(
+                      width: width,
+                      height: height,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsScreen(
+                                      categoryName: "Livestock",
+                                    )),
+                          );
+                        },
+                        shape: CircleBorder(),
+                        child: Icon(
+                          FontAwesomeIcons.horseHead,
+                          color: kPrimaryAccentColor,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  BodyText(textBody: "Livestock"),
-                ],
-              ),
-              SizedBox(height: 10),
-              Column(
-                children: [
-                  Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductsScreen(
-                                    categoryName: "Dairy",
-                                  )),
-                        );
-                      },
-                      shape: CircleBorder(),
-                      child: Icon(
-                        FontAwesomeIcons.glassWhiskey,
-                        color: kPrimaryAccentColor,
+                    SizedBox(height: 5),
+                    BodyText(textBody: "Livestock"),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Column(
+                  children: [
+                    Container(
+                      width: width,
+                      height: height,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsScreen(
+                                      categoryName: "Dairy",
+                                    )),
+                          );
+                        },
+                        shape: CircleBorder(),
+                        child: Icon(
+                          FontAwesomeIcons.glassWhiskey,
+                          color: kPrimaryAccentColor,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  BodyText(textBody: "Dairy"),
-                ],
-              ),
-            ],
+                    SizedBox(height: 5),
+                    BodyText(textBody: "Dairy"),
+                  ],
+                ),
+              ],
+            ),
           ),
-          Column(
-            children: <Widget>[
-              Column(
-                children: [
-                  Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductsScreen(
-                                    categoryName: "Meat",
-                                  )),
-                        );
-                      },
-                      shape: CircleBorder(),
-                      child: Icon(
-                        FontAwesomeIcons.drumstickBite,
-                        color: kPrimaryAccentColor,
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Column(
+                  children: [
+                    Container(
+                      width: width,
+                      height: height,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsScreen(
+                                      categoryName: "Meat",
+                                    )),
+                          );
+                        },
+                        shape: CircleBorder(),
+                        child: Icon(
+                          FontAwesomeIcons.drumstickBite,
+                          color: kPrimaryAccentColor,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  BodyText(textBody: "Meat"),
-                ],
-              ),
-              SizedBox(height: 10),
-              Column(
-                children: [
-                  Container(
-                    width: width,
-                    height: height,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
-                    child: RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProductsScreen(
-                                    categoryName: "Crop",
-                                  )),
-                        );
-                      },
-                      shape: CircleBorder(),
-                      child: Icon(
-                        FontAwesomeIcons.leaf,
-                        color: kPrimaryAccentColor,
+                    SizedBox(height: 5),
+                    BodyText(textBody: "Meat"),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Column(
+                  children: [
+                    Container(
+                      width: width,
+                      height: height,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Color(0xFFF2F3F7)),
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductsScreen(
+                                      categoryName: "Crop",
+                                    )),
+                          );
+                        },
+                        shape: CircleBorder(),
+                        child: Icon(
+                          FontAwesomeIcons.leaf,
+                          color: kPrimaryAccentColor,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  BodyText(textBody: "Crop"),
-                ],
-              ),
-            ],
+                    SizedBox(height: 5),
+                    BodyText(textBody: "Crop"),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
