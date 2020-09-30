8982d2e322ef2c0463f2e58b36e58629836aeec1
zubariaayub634 <zubariaayub634@gmail.com>
2020-09-30 09:49:55 +0500

    Adds popup to update status

--- lib/screens/order/orderDetails_seller.dart
+++ lib/screens/order/orderDetails_seller.dart
@@ -9,6 +9,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:ugao/components/h2.dart';
 import 'package:ugao/components/h3.dart';
 import 'package:ugao/constants.dart';
+import'package:ugao/components/rounded_drop_down.dart';
 
 class OrderDetailsSeller extends StatefulWidget {
   @override
@@ -25,7 +26,7 @@ class _OrderDetailsSellerState extends State<OrderDetailsSeller> {
     "Shipped/Ready",
     "Delivered/Picked Up",
   ];
-  String newStatus = "";
+  String newStatus;
 
   @override
   Widget build(BuildContext context) {
@@ -75,6 +76,71 @@ class _OrderDetailsSellerState extends State<OrderDetailsSeller> {
                 ],
               ),
             ),
+          ),
+          ButtonLoading(
+            labelText: "Edit Status",
+            onTap: () {
+              Alert(
+                  context: context,
+                  title: "Select new status",
+                  style: AlertStyle(
+                    titleStyle: H2TextStyle(color: kPrimaryAccentColor),
+                  ),
+                  content: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 10,
+                      ),
+                      DropdownButton<String>(
+                        isExpanded: true,
+                        isDense: false,
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                        hint: Text("Account Type",
+                            style: TextStyle(fontSize: 15.5)),
+                        value: newStatus,
+                        onChanged: (String value) {
+                          setState(() {
+                            newStatus = value;
+                          });
+                        },
+                        items: orderStatuses.map((String user) {
+                          return DropdownMenuItem<String>(
+                            value: user,
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  user,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 15.5,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ButtonLoading(
+                        onTap: () async {},
+                        labelText: 'Update Status',
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                  buttons: [
+                    DialogButton(
+                      color: Colors.white,
+                      height: 0,
+                    ),
+                  ]).show();
+            },
           ),
         ],
       ),
