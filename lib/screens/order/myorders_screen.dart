884ea34de51160c083a1eec109327ebeb4b88bb3
mishaalnaeem <mishaalnaeem@gmail.com>
2020-09-30 08:33:07 +0500

    Orders List using Firebase

--- lib/screens/order/myorders_screen.dart
+++ lib/screens/order/myorders_screen.dart
@@ -8,6 +8,7 @@ import 'package:ugao/Classes/Order_Model.dart';
 import 'package:ugao/Classes/Cart_Product_Model.dart';
 import 'package:ugao/Providers/general_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:ugao/Classes/User_Model.dart';
 
 class MyOrders extends StatefulWidget {
   @override
@@ -15,47 +16,20 @@ class MyOrders extends StatefulWidget {
 }
 
 class _MyOrdersState extends State<MyOrders> {
-  ///=====temporary order list - remove once backend implemented========
-  final orders_test = [
-    {
-      "orderID": 1,
-      "productName": "Fertilizer",
-      "status": "Shipped",
-      "service": "Delivery",
-      "address": "House",
-      "qty": 2,
-      "payment": "COD",
-      "date": "9/28/2020"
-    },
-    {
-      "orderID": 2,
-      "productName": "Apple",
-      "status": "Delivered",
-      "service": "Delivery",
-      "address": "House",
-      "qty": 5,
-      "payment": "COD",
-      "date": "9/28/2020"
-    },
-    {
-      "orderID": 3,
-      "productName": "Milk",
-      "status": "Ready",
-      "service": "Pick Up",
-      "address": "House",
-      "qty": 1,
-      "payment": "COD",
-      "date": "9/28/2020"
-    }
-  ];
   @override
   Widget build(BuildContext context) {
     /*uncomment this line and fetch all orders against user 
     List<Order> orders=*/
     //Size size = MediaQuery.of(context).size;
+    User user =
+        Provider.of<General_Provider>(context, listen: false).get_user();
     Provider.of<General_Provider>(context, listen: false).fetch_orders();
-    List<Order> orders =
+    List<Order> all_orders =
         Provider.of<General_Provider>(context, listen: false).orders;
+    List<Order> orders = [];
+    for (var i = 0; i < all_orders.length; i++) {
+      if (all_orders[i].customerID == user.cnic) orders.add(all_orders[i]);
+    }
     return Scaffold(
         appBar: AppBarPageName(
           pageName: 'My Orders',
@@ -81,11 +55,13 @@ class _MyOrdersState extends State<MyOrders> {
                           padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
                           child: Row(
                             children: <Widget>[
-                              Text(
-                                "Order Number: " + orders[index].orderID,
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold),
+                              Expanded(
+                                child: Text(
+                                  "Order Number: " + orders[index].orderID,
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
                               ),
                               Spacer(),
                             ],
