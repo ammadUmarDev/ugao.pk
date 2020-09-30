15c297f3352d58eb579d7870edc76c50001de0ad
ammadUmarDev <ammad.umar@outlook.com>
2020-09-30 11:29:55 +0500

    Makes stuff better

--- lib/screens/order/orderDetails_seller.dart
+++ lib/screens/order/orderDetails_seller.dart
@@ -20,43 +20,45 @@ class _OrderDetailsSellerState extends State<OrderDetailsSeller> {
       appBar: AppBarPageName(
         pageName: 'Order Details',
       ),
-      body: Card(
-        child: ListTile(
-          // Leading Section
-          leading: (widget.order.product.prodImage == null)
-              ? Text("No Image Found")
-              : new Image.network(
-                  widget.order.product.prodImage,
-                  width: 80.0,
-                  height: 80.0,
-                ),
-          title: Expanded(
-            child: H3(
-              textBody: "OrderID: \n" + widget.order.orderID,
+      body: SafeArea(
+        child: Card(
+          child: ListTile(
+            // Leading Section
+            leading: (widget.order.product.prodImage == null)
+                ? Text("No Image Found")
+                : new Image.network(
+                    widget.order.product.prodImage,
+                    width: 80.0,
+                    height: 80.0,
+                  ),
+            title: Expanded(
+              child: H3(
+                textBody: "OrderID: \n" + widget.order.orderID,
+              ),
             ),
-          ),
-          subtitle: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              //Row inside Column
-              SizedBox(height: 5),
-              H3(textBody: "Service: " + widget.order.service),
-              H3(textBody: "Payment: " + widget.order.paymentMethod),
-              H3(
-                  textBody:
-                      "Quantity: " + widget.order.productQuantity.toString()),
-              H3(
-                  textBody: "Total Price: PKR " +
-                      (widget.order.productQuantity *
-                              widget.order.product.price)
-                          .toString()),
-              H3(textBody: "Customer Address: " + widget.order.address),
-              H3(
-                  textBody:
-                      "Customer Contact: " + widget.order.customer.phone_no),
-              H3(textBody: "Status: " + widget.order.status),
-            ],
+            subtitle: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                //Row inside Column
+                SizedBox(height: 5),
+                H3(textBody: "Service: " + widget.order.service),
+                H3(textBody: "Payment: " + widget.order.paymentMethod),
+                H3(
+                    textBody:
+                        "Quantity: " + widget.order.productQuantity.toString()),
+                H3(
+                    textBody: "Total Price: PKR " +
+                        (widget.order.productQuantity *
+                                widget.order.product.price)
+                            .toString()),
+                H3(textBody: "Customer Address: " + widget.order.address),
+                H3(
+                    textBody:
+                        "Customer Contact: " + widget.order.customer.phone_no),
+                H3(textBody: "Status: " + widget.order.status),
+              ],
+            ),
           ),
         ),
       ),
