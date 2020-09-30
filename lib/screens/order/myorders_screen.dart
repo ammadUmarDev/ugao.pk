536a5062e595e72a8756b3ac0b942a79800da25e
zubariaayub634 <zubariaayub634@gmail.com>
2020-09-30 12:50:20 +0500

    Refines UI

--- lib/screens/order/myorders_screen.dart
+++ lib/screens/order/myorders_screen.dart
@@ -1,15 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 //my imports
 import 'package:ugao/components/appbar.dart';
 import 'package:ugao/components/body_text.dart';
 import 'package:ugao/components/h2.dart';
 import 'package:ugao/components/h3.dart';
-import 'package:ugao/screens/order/orders_details.dart';
 import '../../constants.dart';
 import 'package:ugao/Classes/Order_Model.dart';
 import 'package:ugao/Providers/general_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:ugao/Classes/User_Model.dart';
+import 'package:ugao/screens/order/orders_details.dart';
 
 class MyOrders extends StatefulWidget {
   @override
@@ -19,81 +20,125 @@ class MyOrders extends StatefulWidget {
 class _MyOrdersState extends State<MyOrders> {
   @override
   Widget build(BuildContext context) {
-    /*uncomment this line and fetch all orders against user 
-    List<Order> orders=*/
-    //Size size = MediaQuery.of(context).size;
     User user =
-        Provider.of<General_Provider>(context, listen: false).get_user();
-    Provider.of<General_Provider>(context, listen: false).fetch_orders();
-    List<Order> all_Orders =
-        Provider.of<General_Provider>(context, listen: false).orders;
-    List<Order> orders = [];
-    for (var i = 0; i < all_Orders.length; i++) {
-      if (all_Orders[i].customerID == user.cnic &&
-          isNotInOrders(orders, all_Orders[i].orderID))
-        orders.add(all_Orders[i]);
-    }
-    return Scaffold(
-        appBar: AppBarPageName(
-          pageName: 'My Orders',
-        ),
+    Provider.of<General_Provider>(context, listen: false).get_user();
+    Widget allOrders = StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance
+            .collection("Orders")
+            .where('customerID', isEqualTo: user.cnic)
+            .orderBy("creationTimestamp",descending: true)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return H3(textBody: 'Error in fetching orders: ${snapshot.error}');
+          }
 
-        //============ORDERS LIST=============
-        body: ListView.builder(
-          itemCount: orders.length,
-          itemBuilder: (context, index) {
-            return Container(
-              /*decoration:
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return H3(textBody: 'Not connected to the Stream or null');
+
+            case ConnectionState.waiting:
+              return H3(textBody: 'Awaiting for interaction');
+
+            case ConnectionState.active:
+              print("Stream has started but not finished");
+
+              var totalOrdersCount = 0;
+              List<DocumentSnapshot> orders;
+
+              if (snapshot.hasData) {
+                orders = snapshot.data.documents;
+                totalOrdersCount = orders.length;
+
+                if (totalOrdersCount > 0) {
+                  return new ListView.builder(
+                    itemCount: orders.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        /*decoration:
                   BoxDecoration(borderRadius: BorderRadius.circular(1.0)),*/
-              child: Card(
-                color: kPrimaryLightColor,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => orderDetails(
-                                orderID: orders[index].orderID,
-                              )),
-                    );
-                  },
-                  highlightColor: kPrimaryColor,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        SizedBox(height: 10),
-                        H2(
-                          textBody: "Order Number: " + orders[index].orderID,
+                        child: Card(
+                          color: kPrimaryLightColor,
+                          child: InkWell(
+                            onTap: () async {
+                              Order order = Order();
+                              await order.fromFirebaseDocument(orders[index]);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => OrderDetails(
+                                        order: order,
+                                      )));
+                            },
+                            highlightColor: kPrimaryColor,
+                            child: Padding(
+                              padding:
+                              const EdgeInsets.symmetric(horizontal: 10),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  SizedBox(height: 10),
+                                  H2(
+                                    textBody: "Order Number: " +
+                                        orders[index]["orderID"].toString(),
+                                  ),
+                                  SizedBox(height: 5),
+                                  H3(
+                                    textBody: "Timestamp: " +
+                                        orders[index]["creationTimestamp"]
+                                            .toString()
+                                            .substring(0, 19),
+                                  ),
+                                  SizedBox(height: 5),
+                                  H3(
+                                    textBody: "Service Type: " +
+                                        orders[index]["service"].toString(),
+                                  ),
+                                  SizedBox(height: 5),
+                                  H3(
+                                    textBody: "Customer Address: " +
+                                        orders[index]["address"].toString(),
+                                  ),
+                                  Divider(),
+                                  SizedBox(height: 5),
+                                  BodyText(
+                                      textBody: "Tap to view more details"),
+                                  SizedBox(height: 10),
+                                ],
+                              ),
+                            ),
+                          ),
                         ),
-                        SizedBox(height: 5),
-                        H3(
-                          textBody: "Service Type: " + orders[index].service,
-                        ),
-                        SizedBox(height: 5),
-                        H3(
-                          textBody: "Status: " + orders[index].status,
-                        ),
-                        Divider(),
-                        SizedBox(height: 5),
-                        BodyText(textBody: "Tap to view more details"),
-                        SizedBox(height: 10),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            );
-          },
-        ));
-  }
-}
+                      );
+                    },
+                  );
+                }
+              }
+              return Center(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50.0),
+                      ),
+                      H3(
+                        textBody: "No orders found.",
+                      )
+                    ],
+                  ));
 
-bool isNotInOrders(List o, String orderId) {
-  for (var j = 0; j < o.length; j++) {
-    if (orderId == o[j].order) return false;
-  }
-  return true;
-}
+            case ConnectionState.done:
+              return H3(textBody: 'All orders fetched');
+          }
+
+          return Container(
+            child: H3(textBody: "No orders found."),
+          );
+        });
+    return Scaffold(
+      appBar: AppBarPageName(
+        pageName: 'Orders Recieved',
+      ),
+      body: allOrders,
+    );
+  }}
